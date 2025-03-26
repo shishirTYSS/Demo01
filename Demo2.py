859fdb809bfc64ab320ea9394bd3cfcdc4b9d45b
shishir1599 <shishir.u@testyantra.in>
2025-03-26 12:42:19 +0530

    ***

--- Demo2.py
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver.chrome.webdriver import WebDriver as Chrome
-
-driver=Chrome()
-driver.get("https://demowebshop.tricentis.com/")
-elements=driver.find_elements("xpath","//div[@class='column customer-service']//a")
-for element in elements:
-    print(element.text)
